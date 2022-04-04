9f43618af3ed6ff54f712a3778b23d4847d37a27
Gourav Singh <gouravsingh510gmail.com@Gouravs-MacBook-Air.local>
2022-04-05 00:11:23 +0530

    update

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,4 +1,4 @@
-def skipRemainingStages ='false'
+def skipRemainingStages ='true'
 pipeline {
     agent any
 	parameters {
@@ -15,21 +15,21 @@ pipeline {
       when  {expression {prams.pr_scan ==true || prams.daily_scan ==true}}
       steps {
           echo 'this is pr or daily scan '
-          skipRemainingStages = 'true'
+          skipRemainingStages = 'false'
           echo "${skipRemainingStages}"
       }
     }
     stage ('daily_stage') {
-      when { expression {params.daily_scan ==true && skipRemainingStages != true }}
+      when { expression {params.daily_scan ==true && skipRemainingStages == false }}
       steps {
           echo 'this is only daily scan'
-          skipRemainingStages = 'true'
+          skipRemainingStages = 'false'
           echo "${skipRemainingStages}"
 
       }
     }
     stage ('deply_stage') {
-      when  (skipRemainingStages != true )
+      when  (skipRemainingStages == true )
       steps {
           echo 'this is deply'
           echo "${skipRemainingStages}"
--- abc.groovy
+++ abc.groovy
@@ -99,3 +99,61 @@ pipeline {
 	}
 }
 =================================================
+def skipRemainingStages ='true'
+pipeline {
+    agent any
+	parameters {
+        choice(choices: ['true' , 'false'],name: 'pr_scan')
+        choice(choices: ['true' , 'false'],name: 'daily_scan')
+	}
+  stages {
+    stage ('common_steps') {
+      steps {
+        echo 'this is common steps'
+      }
+    }
+    stage ('pr_stage') {
+      //when  {expression {prams.pr_scan ==true || prams.daily_scan == true}}
+      steps {
+          script {
+             echo 'this is pr or pr scan '
+             if (prams.pr_scan == true || prams.daily_scan == true) {
+             skipRemainingStages = 'false'
+             print skipRemainingStages = ${skipRemainingStages}
+             echo "${skipRemainingStages}"
+            }else {
+                echo 'no if'
+            }
+          }
+        }
+    }
+    stage ('daily_stage') {
+      //when { expression {params.daily_scan ==true && skipRemainingStages == false }}
+      steps {
+         script{
+            echo 'this is pr or daily scan '
+             if (params.daily_scan ==true && skipRemainingStages == false) {
+             skipRemainingStages = 'false'
+             print skipRemainingStages = ${skipRemainingStages}
+             echo "${skipRemainingStages}"
+            }else {
+                echo 'no if'
+            }
+          }
+        }
+    }
+    stage ('deply_stage') {
+      //when  {expression {skipRemainingStages == true }}
+      steps {
+          script {
+              if (skipRemainingStages == true) {
+                echo 'this is deply'
+                echo "${skipRemainingStages}"
+              }else {
+                echo 'no if'
+            }
+          }
+        }
+    }
+  }
+}
